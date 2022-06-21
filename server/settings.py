8795a23f8020e9af2f5c41db0b5cd0bf313ece0c
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-06-21 21:32:51 +0530

    changed the token validity

--- server/settings.py
+++ server/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = "/media/"
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
